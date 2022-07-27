17788c28c4223c42909fa52fbd5155d1a1681b76
Vijay <capvijju24@gmail.com>
2022-07-27 23:18:42 +0530

    task.js commit

--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,7 @@
 const Task = ({ tasks, del }) => {
   return tasks.map((task) => {
     return (
-      <div id={task.id} key={task.id} className="task_bar">
+      <div key={task.id} className="task_bar">
         <p>{task.name}</p>
         <button id={task.id} onClick={del}>
           DELETE
